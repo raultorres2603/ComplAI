bd2672ecc0c4242764dce7fea9ce8bfeb5af32cc
Raul Torres <55553058+raultorres2603@users.noreply.github.com>
2026-02-06 19:05:36 +0100

    refactor: remove unused Micronaut Jackson dependency and add HomeDto class

--- src/main/java/cat/complai/home/HomeController.java
+++ src/main/java/cat/complai/home/HomeController.java
@@ -2,6 +2,7 @@ package cat.complai.home;
 
 import io.micronaut.http.annotation.Controller;
 import io.micronaut.http.annotation.Get;
+import io.micronaut.http.annotation.Produces;
 
 import java.util.logging.Logger;
 
@@ -11,6 +12,7 @@ public class HomeController {
     private final Logger logger = Logger.getLogger(HomeController.class.getName());
 
     @Get("/")
+    @Produces("application/json")
     public String index() {
         logger.info("Accessed home page");
         return "Hello, welcome to ComplAI! This is the home page.";
