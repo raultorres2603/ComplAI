ccded0eead21214a699155b5830717f5e5388b54
Raul Torres <raultorres2603@gmail.com>
2026-05-16 18:56:38 +0200

    let's see

--- src/main/java/cat/complai/services/stadistics/StadisticsService.java
+++ src/main/java/cat/complai/services/stadistics/StadisticsService.java
@@ -6,9 +6,6 @@ import java.time.YearMonth;
 import java.time.ZoneId;
 import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
